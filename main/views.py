b3177df8d36c71ad3b2ca29e1fedabb9b6ecad37
Anton nout <webadmin87@gmail.com>
2015-10-25 21:04:27 +0300

    возможность использовать шаблонизатор в тексте страниц

--- main/views.py
+++ main/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Page
+from django.template import Template, Context
 
 
 def index(request, slug='main'):
 
     p = get_object_or_404(Page, slug=slug)
 
-    return render(request, 'main/pages/index.html', {'page': p})
+    text = Template(p.text).render(Context())
+
+    return render(request, 'main/pages/index.html', {'page': p, 'text': text})
 
 
