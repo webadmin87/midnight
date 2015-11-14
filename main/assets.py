e6031ef3b3b89f5bd1dfb9682400fb3a98c0928a
Anton nout <webadmin87@gmail.com>
2015-11-14 12:34:12 +0300

    правка ассетов

--- main/assets.py
+++ main/assets.py
@@ -2,5 +2,5 @@ from django_assets import Bundle, register as asset_register
 
 js = Bundle('jquery/dist/jquery.min.js', 'fancybox/source/jquery.fancybox.pack.js', 'main/js/ajaxForm.js', 'main/js/comments.js', 'main/js/main.js')
 css = Bundle('fancybox/source/jquery.fancybox.css', 'main/css/comments.css')
-asset_register('js_all', js)
-asset_register('css_all', css)
+asset_register('js_main', js)
+asset_register('css_main', css)
