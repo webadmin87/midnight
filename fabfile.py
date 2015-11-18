adbf629eb1bd5bcc04785d4ce3457ff2a2a2ea5e
Anton nout <webadmin87@gmail.com>
2015-11-18 21:39:30 +0300

    добавляем настройки автоматического деплоя

---
+++ fabfile.py
@@ -0,0 +1,54 @@
+from fabric.api import *
+from contextlib import contextmanager
+
+env.hosts = ['host_name']
+env.user = 'user_name'
+env.keyfile = ['$HOME/.ssh/private_key']
+env.directory = '/path/to/project'
+env.activate = 'source /path/to/virtualenv/bin/activate'
+env.uwsgi_pid = '/tmp/project_name.pid'
+
+
+@contextmanager
+def virtualenv():
+    with cd(env.directory):
+        with prefix(env.activate):
+            yield
+
+
+def pull_data():
+    with virtualenv():
+        run('git pull origin master')
+
+
+def pip_install():
+    with virtualenv():
+        run('pip install -r requirements')
+
+
+def bower_install():
+    with virtualenv():
+        run('bower install')
+
+
+def collect_static():
+    with virtualenv():
+        run('./manage.py collectstatic --noinput')
+
+
+def migrate():
+    with virtualenv():
+        run('./manage.py migrate')
+
+
+def reload_uwsgi():
+    run('uwsgi --reload '+env.uwsgi_pid)
+
+
+def deploy():
+    execute(pull_data)
+    execute(pip_install)
+    execute(bower_install)
+    execute(collect_static)
+    execute(migrate)
+    execute(reload_uwsgi)
