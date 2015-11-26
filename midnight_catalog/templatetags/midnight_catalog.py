319fae45cce8a149d796e38f0e0a09c40f71a79a
Anton office <webadmin87@gmail.com>
2015-11-26 13:39:29 +0300

    фильтр для валют

--- midnight_catalog/templatetags/midnight_catalog.py
+++ midnight_catalog/templatetags/midnight_catalog.py
@@ -1,4 +1,6 @@
 from django import template
+from django.contrib.humanize.templatetags.humanize import intcomma
+from django.conf import settings
 
 register = template.Library()
 
@@ -17,4 +19,16 @@ def param_title(param_values, slug):
         if val.param.slug == slug:
             return val.param.title
     return None
+
+
+@register.filter()
+def currency(money):
+    decimals = getattr(settings, 'MIDNIGHT_CATALOG_DECIMALS', 2)
+    money = round(float(money), decimals)
+    symbol = getattr(settings, 'MIDNIGHT_CATALOG_CURRENCY', 'руб')
+    if decimals > 0:
+        formatted = (str("%0."+str(decimals)+"f") % money)[-decimals-1:]
+    else:
+        formatted = ""
+    return "%s%s %s" % (intcomma(int(money)), formatted, symbol)
 
