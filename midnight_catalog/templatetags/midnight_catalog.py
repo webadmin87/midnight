f41943dff2611729f7264de383ec98c46d9eb16f
Anton office <webadmin87@gmail.com>
2015-11-26 13:02:36 +0300

    улучшаем каталог

--- midnight_catalog/templatetags/midnight_catalog.py
+++ midnight_catalog/templatetags/midnight_catalog.py
@@ -9,3 +9,12 @@ def param_value(param_values, slug):
         if val.param.slug == slug:
             return val.value
     return None
+
+
+@register.simple_tag()
+def param_title(param_values, slug):
+    for val in param_values:
+        if val.param.slug == slug:
+            return val.param.title
+    return None
+
