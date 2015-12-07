408e7362c7d1513e3e577e027fe1cf786daa6a67
Anton office <webadmin87@gmail.com>
2015-12-07 16:37:05 +0300

    подсчет количества товаров в списке категорий каталога

--- midnight_catalog/templatetags/midnight_catalog.py
+++ midnight_catalog/templatetags/midnight_catalog.py
@@ -1,6 +1,7 @@
 from django import template
 from django.contrib.humanize.templatetags.humanize import intcomma
 from django.conf import settings
+from django.db.models import Count, Case, When, Value, IntegerField
 
 from midnight_catalog.models import Section
 from midnight_main.services import mark_current_menus
@@ -37,12 +38,13 @@ def currency(money):
 
 
 def catalog_sections(context, slug=None, level=2, **kwargs):
+    count_products = Count(Case(When(product__active=True, then=Value(1)), output_field=IntegerField()))
     if slug is None:
-        sections = Section.objects.published().all()
+        sections = Section.objects.annotate(product__count=count_products).published().all()
         max_level = level - 1
     else:
         section = Section.objects.get(slug=slug)
-        sections = section.get_descendants().all()
+        sections = section.get_descendants().annotate(product__count=count_products).all()
         max_level = section.level + level
     mark_current_menus(sections, context['request'].path_info)
     return {'sections': sections, 'max_level': max_level, 'data': kwargs}
